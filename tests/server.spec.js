332ad23bce47b7c48eafe35121423f069cc6b1a5
Steve Schwarzmann <steven.schwarzmann@gmail.com>
2018-04-04 11:09:53 -0700

    test commit

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,14 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page displays content', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('The content would go here');
+        done();
+      });
+    });
 })
